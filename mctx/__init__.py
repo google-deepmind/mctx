c0be0eb036522222ae834f8440013734fc8a252e
Ivo Danihelka <danihelka@google.com>
2023-11-24 02:41:42 -0800

    Increase the mctx version number to 0.0.4.
    PiperOrigin-RevId: 585043158

--- mctx/__init__.py
+++ mctx/__init__.py
@@ -37,7 +37,7 @@ from mctx._src.qtransforms import qtransform_completed_by_mix_value
 from mctx._src.search import search
 from mctx._src.tree import Tree
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 __all__ = (
     "ChanceRecurrentFnOutput",
